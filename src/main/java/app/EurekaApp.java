4f05e0a11719705948ac613521fcdcdd773cc9f2
xiaodi <823497477@qq.com>
2019-10-28 13:10:13 +0800

    111

---
+++ src/main/java/app/EurekaApp.java
@@ -0,0 +1,17 @@
+package app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+//eureka声明
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApp 
+{
+    public static void main( String[] args )
+    {
+        new SpringApplicationBuilder(EurekaApp.class).web(true).run(args);
+    }
+}
